1f08bef7cb1dd5a04723134f59d29ea32a7aabe1
John McLear <john@mclear.co.uk>
2013-01-13 22:39:21 +0000

    fix match where it was only returning 1

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ exports.registerRoute = function (hook_name, args, cb) {
       async.forEachSeries(revs, function(revNo, callback){
         pad.getInternalRevisionAText(revNo, function(junk, text)
         {
-          if(text.text.match(searchString, 'g')){ // If we find a match
-            var count = text.text.match(searchString, 'g').length;
+          if(text.text.match(searchString)){ // If we find a match
+            var count = text.text.match(new RegExp(searchString, 'g')).length; // Thanks to rKnLa
           }else{ // If we don't find amatch
             var count = 0;
           }
