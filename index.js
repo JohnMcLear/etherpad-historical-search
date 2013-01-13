81905fec3b7ca96e8862e1ad2ecc18458a77f1bf
John McLear <john@mclear.co.uk>
2013-01-13 00:54:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+var eejs = require('ep_etherpad-lite/node/eejs')
+  , padManager = require('ep_etherpad-lite/node/db/PadManager')
+  , log4js = require('ep_etherpad-lite/node_modules/log4js')
+  , async = require('ep_etherpad-lite/node_modules/async')
+  , logger = log4js.getLogger("plugin:historicalsearch")
+;
+
+// testing on /test
+exports.registerRoute = function (hook_name, args, cb) {
+  args.app.get('/historicalSearch', function(req, res) {
+  // args.app.get('/test', function(req, res) { // Leaving in for further dev & testing
+
+    var searchString = req.query["query"];
+    var padId = req.query["padId"];
+
+    padId = "test"; // DELETE ME
+    searchString = "test"; // DELETE ME
+
+    // Holding values
+//    var result = {0:1,1:1,2:2,3:2,4:2,5:3,6:3,7:4,8:4,9:5,10:3,11:2,12:1,13:0,13:0}; // example results
+//    res.send(JSON.stringify(result)); // test sent TODO remove me
+
+    var result = {};
+
+    padManager.getPad(padId, function(err, _pad){
+      pad = _pad;
+      var num = pad.getHeadRevisionNumber(); // Gets the # of revisions in a pad
+      var result = {}; // The object we store the result in before sending back to client
+      var revs = []; // The array we will store the revs in for this pad
+
+      for (var i=0; i <= num; i++) { // Push each revision # to an array
+        revs.push(i);
+      }
+      async.forEachSeries(revs, function(revNo, callback){
+        pad.getInternalRevisionAText(revNo, function(junk, text)
+        {
+          if(text.text.match(/test/g)){ // If we find a match
+            var count = text.text.match(/test/g).length;
+          }else{ // If we don't find amatch
+            var count = 0;
+          }
+          result[revNo] = count; // Write the count to a result object
+
+          // console.warn("String exists " + count + " times in revNo " +revNo);
+          callback(err);
+        });
+
+      }, function(err){
+        res.send(JSON.stringify(result));
+      });
+
+    });
+  });
+};
+
+
+exports.eejsBlock_timesliderBody = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_historicalsearch/static/js/historySearch.js");
+  return cb();
+}
+
---
+++ static/js/historySearch.js
@@ -0,0 +1,96 @@
+<script type="text/javascript">
+// Most of this is Bad Javascript.  It is bad because none of the hooks etc. I needed at the time were available.
+// Apologies..  J
+
+
+// A global object for search Results as I don't know a better way to make it work on resize
+var historicalSearchResults = {};
+
+// Stop the editbar being cropped in IE
+$('#editbar').css({"width":"auto"});
+
+
+
+// Prepare the UI (usually done w/ EJS)	
+$('.editbarright ul').prepend("<li onclick='$(\".historicalSearch\").toggle();return false;'> \
+  <a id=\"exportlink\" title=\"Search history of pad\"> \
+  <div class=\"buttonicon\" style=\"font-size:14px;color:black;width:60px;background-image:none;\">Search</div></a> \
+  </li></ul> \
+");
+
+$('#timeslider-top').append("<div class='historicalSearch popup' style='float:right;display:none;'> \
+  Historical Search (Searches through entire history of document) \
+  <form id='historicalSearchForm' style='display:inline;'> \
+  <input id='historicalSearchQuery' type='text'><input type='submit' value='Search' style='padding:10px'></form> \
+  </div> \
+");
+
+
+
+// Set up listeners
+
+// Prepare a submit function to send the search query to the server
+$('#historicalSearchForm').submit(function(event){ // Send query to server
+  event.preventDefault(); // prevent default submit action
+  var query = $('#historicalSearchQuery').val(); // get the input value
+
+  // Due to limitations with the timeslider on the plugin framework we have to do an AJAX call here
+  data = {
+    "padId" : "test" ,
+    "type" : "historicalSearch",
+    "query" : query
+  }
+
+  var url = '/historicalSearch?padID='+data.padId+'&query='+data.query;
+
+  $.get(url, function(results, query){
+    historicalSearchResults = results;
+    updateTimesliderWithSearchResults(historicalSearchResults, query);
+  });
+
+});
+
+$(window).resize(function() { // On browser resize draw the count results
+  updateTimesliderWithSearchResults()
+});
+
+// End of listeners
+
+
+
+function updateTimesliderWithSearchResults(){ // Updates the UI with results
+  // Clean up first
+  $('.countBlock').remove();
+
+  results = JSON.parse(historicalSearchResults);
+
+  // total # of revs
+  var numberOfRevs = Object.size(results);
+
+  // length of bar
+  var sliderBarLength = parseFloat($('#ui-slider-bar').width() -8);
+
+  // The amount of space assigned to each rev
+  var pxPerRev = sliderBarLength / numberOfRevs;
+
+  $.each(results, function(index, value){ // For each revision show the count
+    var height = value * 5;
+    var left = (index * pxPerRev)+8;
+    var elementId = "countIndex"+index;
+    var countBlock = "<div id="+elementId+" title=\"Search string appeared "+value+" times\"class='countBlock' pos="+index+" \
+    style=\"background-color:black;bottom:8px;max-height:30px;opacity:.3;position:absolute;height: \
+    "+height+"px;width:"+pxPerRev+"px;left:"+left+"px\"></div>";
+    $('#timeslider-slider').append(countBlock);
+ 
+    // $('#'+elementId).fadeIn(); // Commented out because it will prolly cause bugs
+  });
+}
+
+Object.size = function(obj) { // Get the size of an object when .length doesn't work
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+</script>
